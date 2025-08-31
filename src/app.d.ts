89aaa632e0cf01e7d17673f99492eef4f35f6377
aseli <aseli4488@gmail.com>
2025-08-31 14:37:05 +0900

    Add: types for platform

--- src/app.d.ts
+++ src/app.d.ts
@@ -1,5 +1,8 @@
 // See https://svelte.dev/docs/kit/types#app.d.ts
 // for information about these interfaces
+
+import { DurableObjectNamespace, D1Database } from '@cloudflare/workers-types';
+
 declare global {
 	namespace App {
 		// interface Error {}
