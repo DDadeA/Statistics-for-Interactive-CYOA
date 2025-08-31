fbfa4c23b904ec72de5f0b4074b05134a829f600
aseli <aseli4488@gmail.com>
2025-08-31 14:37:22 +0900

    Add: query functions

--- src/routes/api/registration/+server.ts
+++ src/routes/api/registration/+server.ts
@@ -1,6 +1,16 @@
 import type { RequestHandler } from '@sveltejs/kit';
 
 export async function GET({ request, platform }: { request: Request; platform: App.Platform }) {
-	let result = await platform.env.DB.prepare('SELECT * FROM users LIMIT 5').run();
+	let result = await query(platform, 'SELECT * FROM users LIMIT 5');
+
 	return new Response(JSON.stringify(result));
 }
+
+const query = async (platform: App.Platform, query: string, params?: any[]) => {
+	// @ts-ignore
+	const DB = platform.env.DB;
+	const statement = DB.prepare(query).bind(...(params || []));
+	const result = await statement.all();
+
+	return result;
+};
